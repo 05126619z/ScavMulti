c0a7821f0a5a495e792de13f537c5f214019ff60
dath <dath@dath.org>
2025-10-03 23:38:26 +0200

    ADD world blocks sync using seed and REMOVE old method

--- ScavMulti/Network/Messages/MessageBase.cs
+++ ScavMulti/Network/Messages/MessageBase.cs
@@ -5,11 +5,5 @@ namespace ScavMulti.Network.Messages;
 [Union(0, typeof(Error))]
 [Union(1, typeof(PeerHandshake))]
 [Union(2, typeof(WorldInfo))]
-[Union(3, typeof(ChunkRequest))]
-[Union(4, typeof(ChunkRequestWhole))]
-[Union(5, typeof(ChunkInfo))]
-[Union(6, typeof(ChunkBackgroundRequest))]
-[Union(7, typeof(ChunkBackgroundSpriteNames))]
-[Union(8, typeof(ChunkBackground))]
-[Union(9, typeof(WallHoles))]
+[Union(3, typeof(ModifiedBlocksInfo))]
 public abstract record class MessageBase { }
--- ScavMulti/Network/Messages/Messages.cs
+++ ScavMulti/Network/Messages/Messages.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using MessagePack;
+using UnityEngine;
 
 namespace ScavMulti.Network.Messages;
 
@@ -21,61 +23,11 @@ public record class WorldInfo(
 	[property: Key(2)] uint ChunkSize,
 	[property: Key(3)] float CurrentExperimentPosX,
 	[property: Key(4)] float CurrentExperimentPosY,
-	[property: Key(5)] int BiomeDepth 
-) : MessageBase;
-
-/// <summary>
-/// requests a specific ChunkInfo
-/// </summary>
-[MessagePackObject]
-public record class ChunkRequest(
-	[property: Key(0)] uint X,
-	[property: Key(1)] uint Y
-) : MessageBase;
-
-/// <summary>
-/// requests the entire world chunks as separate ChunkInfoes (columns then rows)
-/// </summary>
-[MessagePackObject]
-public record class ChunkRequestWhole : MessageBase;
-
-/// <param name="Data">a two-dimensional array representing a block matrix (columns then rows)</param>
-[MessagePackObject]
-public record class ChunkInfo(
-	[property: Key(0)] ushort[,] Data
+	[property: Key(5)] UnityEngine.Random.State WorldGenSeed,
+	[property: Key(6)] int BiomeDepth 
 ) : MessageBase;
 
 [MessagePackObject]
-public record class ChunkBackgroundRequest : MessageBase;
-
-[MessagePackObject]
-public record class ChunkBackgroundSpriteNames(
-	[property: Key(0)] string[] Names
+public record class ModifiedBlocksInfo(
+	[property: Key(0)] Dictionary<Vector2Int, ushort> Entries
 ) : MessageBase;
-
-[MessagePackObject]
-public record class ChunkBackground(
-	[property: Key(0)] ChunkBackground.Entry[] Entries
-) : MessageBase
-{
-	[MessagePackObject]
-	public record class Entry(
-		[property: Key(0)] uint X,
-		[property: Key(1)] uint Y,
-		[property: Key(2)] int NameIndex
-	);
-}
-
-[MessagePackObject]
-public record class WallHoles(
-	[property: Key(0)] WallHoles.WallHole[] Entries
-) : MessageBase
-{
-	[MessagePackObject]
-	public record class WallHole(
-		[property: Key(0)] float X,
-		[property: Key(1)] float Y,
-		[property: Key(2)] float Rotation,
-		[property: Key(3)] float WindPitch
-	);
-}
