8f3a42b0363ef0fc4e11f9a642a970366671dec5
dath <dath@dath.org>
2025-10-05 22:45:23 +0200

    ADD preload patcher project

---
+++ ScavMulti.PreloadPatcher/PreloadPatcher.cs
@@ -0,0 +1,37 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace ScavMulti.PreloadPatcher;
+
+// here we're writing stubs that we're gonna hook in the main code
+// this is useful for Unity specific functions like OnDestroy
+// that don't exist in the assembly
+// unfortunately we can't add instance fields because they crash
+// the game immediately
+public static class PreloadPatcher
+{
+    public static IEnumerable<string> TargetDLLs { get; } = new[] {"Assembly-CSharp.dll"};
+
+	static readonly (string typeName, string methodName)[] StubDefinitions = {
+		("BuildingEntity", "OnDestroy"),
+	};
+
+	public static void Patch(AssemblyDefinition assembly)
+    {
+		var voidType = assembly.MainModule.TypeSystem.Void;
+
+		foreach (var tuple in StubDefinitions)
+		{
+			var methodDef = new MethodDefinition(
+				tuple.methodName,
+				MethodAttributes.Public,
+				voidType
+			);
+			methodDef.Body.InitLocals = true;
+			var processor = methodDef.Body.GetILProcessor();
+			processor.Emit(OpCodes.Ret);
+			assembly.MainModule.GetType(tuple.typeName).Methods.Add(methodDef);
+		}
+
+	}
+}
